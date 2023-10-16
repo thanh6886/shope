ce3515d8c3e404190eb4b0db104514cff90af4b4
thanh6886 <phanquangthanh136@gmail.com>
2023-10-17 01:10:44 +0700

    .

--- ShopeeClone/src/main.tsx
+++ ShopeeClone/src/main.tsx
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from 'src/App'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
+const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
